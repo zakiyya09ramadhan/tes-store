f408f8a111540d31316238ed2446da457cfc8e56
zakiyya09ramadhan <zakiyya09ramadhan@gmail.com>
2025-03-05 15:43:28 +0700

    coba tambah header

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
+@section('header')
+    @auth
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Master Barang') }}
+        </h2>
+    @endauth
+@endsection
+
 @section('content')
 <div class="container text-center">
     <h1>Selamat Datang di Sistem Inventaris</h1>
--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/admin', [DashboardController::class, 'admin'])->middleware('role:admin');
